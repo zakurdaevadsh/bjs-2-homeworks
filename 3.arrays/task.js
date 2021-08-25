4125b95f60d6f43c74e82cf3a406dc6055580bd1
Darya Zakurdaeva <zakurdaevadsh@gmail.com>
2021-08-25 22:06:23 +0300

    Homework3

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,15 +1,12 @@
+let arr1 = [8, 1, 2];
+let arr2 = [8, 1, 2];
 function compareArrays(arr1, arr2) {
-  let result;
-
-  // Ваш код
-
-  return result; // boolean
+  return arr1.every((item, idx) => arr2[idx] === item)
 }
+console.log(compareArrays(arr1, arr2))
 
+let arr = [-1, 6, -9, 3, 17];
 function advancedFilter(arr) {
-  let resultArr;
-
-  // Ваш код
-
-  return resultArr; // array
+  return arr.filter(item => item > 0 && item % 3 === 0).map(item => item * 10)
 }
+console.log(advancedFilter(arr))
